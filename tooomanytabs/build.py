b99a68cc46f2d9c8d645071e28b17387bc3ad736
dunk <dunk@inspo.(none)>
2009-11-12 09:38:55 +0000

    add in version number to builds

--- tooomanytabs/build.py
+++ tooomanytabs/build.py
@@ -1,11 +1,18 @@
 import subprocess
 import os
 
+from xml.etree import ElementTree
+
 
 def run():
     if not os.path.exists('dist'):
         os.mkdir('dist')
-    target = 'dist/tooomanytabs@dunkfordyce.xpi'
+
+    install_rdf = ElementTree.fromstring(open('install.rdf').read())
+    version = install_rdf.find('.//{http://www.mozilla.org/2004/em-rdf#}version').text
+
+    target = 'dist/tooomanytabs@dunkfordyce-%s.xpi' % version
+
     if os.path.exists(target):
         os.unlink(target)
     subprocess.call([
